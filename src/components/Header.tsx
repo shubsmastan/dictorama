93ef8f13ed3bc680e51681d2910821ce82de0ef6
Shoaib Mastan <114753586+shubsmastan@users.noreply.github.com>
2025-02-12 18:50:29 +0000

    feat: basic structure

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export function Header() {
+	return (
+		<div className='bg-blue-900 w-full p-3'>
+			<h1 className='text-xl font-bold'>Dictorama</h1>
+		</div>
+	);
+}
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,10 @@
+export function SearchBar() {
+	return (
+		<>
+			<input
+				type='text'
+				placeholder='Start typing a word'
+				className='bg-gray-200 text-black p-2 rounded-md w-full'></input>
+		</>
+	);
+}
