94e1eae8cf917570cd2b8e4357b8a708127dafdc
Shoaib Mastan <114753586+shubsmastan@users.noreply.github.com>
2025-02-12 20:48:02 +0000

    feat: search bar state

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,10 +1,38 @@
+import { useEffect, useState } from 'react';
+
 export function SearchBar() {
+	const [query, setQuery] = useState('');
+	const [debouncedQuery, setDebouncedQuery] = useState(query);
+
+	const handleChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setQuery(e.target.value);
+	};
+
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			setDebouncedQuery(query);
+		}, 500);
+
+		return () => {
+			clearTimeout(timer);
+		};
+	}, [query]);
+
+	useEffect(() => {
+		if (debouncedQuery) {
+			console.log('Debounced query:', debouncedQuery);
+		}
+	}, [debouncedQuery]);
+
 	return (
 		<>
 			<input
 				type='text'
 				placeholder='Start typing a word'
-				className='bg-gray-200 text-black p-2 rounded-md w-full'></input>
+				value={query}
+				onChange={handleChangeSearch}
+				className='bg-gray-200 text-black p-2 rounded-md w-full'
+			/>
 		</>
 	);
 }
