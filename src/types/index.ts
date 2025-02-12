80dff2451f53c0b4038ccd160962cc6b95092d6d
Shoaib Mastan <114753586+shubsmastan@users.noreply.github.com>
2025-02-12 21:15:09 +0000

    chore: update type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,13 @@
 export interface Word {
 	word: string;
-	phonetic: string;
+	phonetic: Phonetics[];
 	meanings: Meaning[];
 	sourceUrls: string;
+}
+
+interface Phonetics {
+	text?: string;
+	audio?: string;
 }
 
 interface Meaning {
